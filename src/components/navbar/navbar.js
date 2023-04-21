48762515d8d13bb3dc0f6fd56fa168ba7a9ed6d3
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-21 16:34:38 +0800

    build navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,7 +1,17 @@
 import classes from './navbar.module.scss';
 
 const Navbar = () => {
-  return <div className={classes.wrapper}>Navbar</div>;
+  return (
+    <div className={classes.wrapper}>
+      <section className={classes.left_block}>
+        <div>logo</div>
+      </section>
+      <section className={classes.right_block}>
+        <div>建案詳情</div>
+        <div>avatar</div>
+      </section>
+    </div>
+  );
 };
 
 export default Navbar;
