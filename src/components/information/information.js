a738298a7bbafe2e98d7428a77fc63824c985bb4
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-22 16:49:09 +0800

    next btn ui

--- src/components/information/information.js
+++ src/components/information/information.js
@@ -34,6 +34,14 @@ const Information = () => {
       <div className={classes.wrapper}>
         <Parking />
       </div>
+
+      <div className={classes.wrapper}>
+        <div>推薦設計風格,實際設計價格將有專人將跟你聯絡</div>
+        <div className={classes.selection}>新增設計風格 +</div>
+      </div>
+
+      <div>仍在考慮？先加入願望清單，或分享給至親好友</div>
+      <div className={classes.selection}>下一步</div>
     </>
   );
 };
