da575b7648f3b404960a2a819d4fb76280daa5e7
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-26 23:54:25 +0800

    fix logo to relative

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,16 +1,17 @@
 import classes from './navbar.module.scss';
 import Image from 'next/image';
 import avatar from '../../../public/avatar.png';
+import icon_logo from '../../../public/icon_logo.png';
 
 const Navbar = () => {
   return (
     <div className={classes.wrapper}>
       <section className={classes.left_block}>
-        <div>logo</div>
+        {/* <Image src={icon_logo} alt="logo" className={classes.logo} /> */}
       </section>
       <section className={classes.right_block}>
         <div>建案詳情</div>
-        <Image src={avatar} alt="avatar" className={classes.image}/>
+        <Image src={avatar} alt="avatar" className={classes.image} />
       </section>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import house_2 from '../../public/house_2.png';
 import house_3 from '../../public/house_3.png';
 import house_4 from '../../public/house_4.png';
 import house_5 from '../../public/house_5.png';
+import icon_logo from '../../public/icon_logo.png';
 import classes from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
+      <Image src={icon_logo} alt="logo" className={classes.logo} />
       <div className={classes.image_wrapper}>
         <Image src={house_1} alt="house_1" className={classes.image} />
         <Image src={house_2} alt="house_2" className={classes.image} />
