6a9ed484a9334806e097283bad15048c468ea268
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-26 16:42:12 +0800

    add ground images

--- src/components/information/information.js
+++ src/components/information/information.js
@@ -5,6 +5,17 @@ import Household from '../household/household';
 import Location from '../location/location';
 import Parking from '../parking/parking';
 import classes from './information.module.scss';
+import Image from 'next/image';
+
+import structure_1 from '../../../public/structure_1.png';
+import structure_2 from '../../../public/structure_2.png';
+import structure_3 from '../../../public/structure_3.png';
+import structure_4 from '../../../public/structure_4.png';
+import ground_1 from '../../../public/ground_1.png';
+import ground_2 from '../../../public/ground_2.png';
+import ground_3 from '../../../public/ground_3.png';
+import ground_4 from '../../../public/ground_4.png';
+import ground_5 from '../../../public/ground_5.png';
 
 const Information = () => {
   return (
@@ -17,14 +28,41 @@ const Information = () => {
       </div>
 
       <div className={classes.wrapper}>
-        images
+        <div className={classes.image_wrapper}>
+          <Image
+            src={structure_1}
+            alt="structure_1"
+            className={classes.image}
+          />
+          <Image
+            src={structure_2}
+            alt="structure_2"
+            className={classes.image}
+          />
+          <Image
+            src={structure_3}
+            alt="structure_3"
+            className={classes.image}
+          />
+          <Image
+            src={structure_4}
+            alt="structure_4"
+            className={classes.image}
+          />
+        </div>
         <Location />
         <Floor />
         <div className={classes.detail_button}>+ 詳細</div>
       </div>
 
       <div className={classes.wrapper}>
-        images
+        <div className={classes.image_wrapper}>
+          <Image src={ground_1} alt="ground_1" className={classes.image} />
+          <Image src={ground_2} alt="ground_2" className={classes.image} />
+          <Image src={ground_3} alt="ground_3" className={classes.image} />
+          <Image src={ground_4} alt="ground_4" className={classes.image} />
+          <Image src={ground_5} alt="ground_5" className={classes.image} />
+        </div>
         <Detail />
         <Location />
         <Household />
