6b8350f6f956576d71e5721c39d1db26f2eaf84d
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-27 03:17:55 +0800

    footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,10 +3,19 @@ import classes from './footer.module.scss';
 const Footer = () => {
   return (
     <div className={classes.wrapper}>
-      <div className={classes.content}>
+      {/* <div className={classes.content}>
         <span className={classes.agree}>同意聲明</span>{' '}
         此網站儲存儲如cookie等數據，以啟用網站基本功能與行銷，個人化及分析功能。
         繼續瀏覽本網站，即表示您同意我們的聲明。 Cookie 政策
+      </div> */}
+      <div>+</div>
+      <div className={classes.small_content}>
+        <p>
+          NT$12,609,900<span>/目前總價</span>
+        </p>
+        <p>
+          NT$31,609<span>/最低月付</span>
+        </p>
       </div>
     </div>
   );
--- src/components/information/information.js
+++ src/components/information/information.js
@@ -82,11 +82,15 @@ const Information = () => {
       </div>
 
       <div className={classes.wrapper}>
-        <div>推薦設計風格,實際設計價格將有專人將跟你聯絡</div>
+        <div className={classes.contact_desc}>
+          推薦設計風格,實際設計價格將有專人將跟你聯絡。
+        </div>
         <div className={classes.selection}>新增設計風格 +</div>
       </div>
 
-      <div>仍在考慮？先加入願望清單，或分享給至親好友</div>
+      <div className={classes.share_desc}>
+        仍在考慮？先加入願望清單，或分享給至親好友
+      </div>
       <div className={classes.next_btn}>下一步</div>
     </div>
   );
