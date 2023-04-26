952d9892449b9c0c650851540ce4b8edb298099c
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-26 16:21:46 +0800

    add image and slide

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import Information from '@/components/information/information';
+import Image from 'next/image';
+import house_1 from '../../public/house_1.png';
+import classes from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
-      <div>images</div>
+      <div className={classes.image_wrapper}>
+        <Image src={house_1} alt="house_1" className={classes.image} />
+        <Image src={house_1} alt="house_1" className={classes.image} />
+      </div>
       <Information />
     </>
   );
