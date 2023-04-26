2b5ee5ea8a52db39a17e84fd01e0cc18edfe5b96
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-26 16:28:24 +0800

    add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 import Information from '@/components/information/information';
 import Image from 'next/image';
 import house_1 from '../../public/house_1.png';
+import house_2 from '../../public/house_2.png';
+import house_3 from '../../public/house_3.png';
+import house_4 from '../../public/house_4.png';
+import house_5 from '../../public/house_5.png';
 import classes from '../styles/Home.module.css';
 
 export default function Home() {
@@ -8,7 +12,10 @@ export default function Home() {
     <>
       <div className={classes.image_wrapper}>
         <Image src={house_1} alt="house_1" className={classes.image} />
-        <Image src={house_1} alt="house_1" className={classes.image} />
+        <Image src={house_2} alt="house_2" className={classes.image} />
+        <Image src={house_3} alt="house_3" className={classes.image} />
+        <Image src={house_4} alt="house_4" className={classes.image} />
+        <Image src={house_5} alt="house_5" className={classes.image} />
       </div>
       <Information />
     </>
