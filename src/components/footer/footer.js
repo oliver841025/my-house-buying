8992736f531ae27d01e99aa6c9729890ef45a39d
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-28 02:32:54 +0800

    1920

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,22 +1,31 @@
+import useWindowSize from '@/hooks/useWindowSize';
 import classes from './footer.module.scss';
 
 const Footer = () => {
+  const size = useWindowSize();
+
   return (
     <div className={classes.wrapper}>
-      {/* <div className={classes.content}>
-        <span className={classes.agree}>同意聲明</span>{' '}
-        此網站儲存儲如cookie等數據，以啟用網站基本功能與行銷，個人化及分析功能。
-        繼續瀏覽本網站，即表示您同意我們的聲明。 Cookie 政策
-      </div> */}
-      <div className={classes.arrow}>𐌡</div>
-      <div className={classes.small_content}>
-        <p>
-          NT$12,609,900<span>/目前總價</span>
-        </p>
-        <p className={classes.monthly_pay}>
-          NT$31,609<span>/最低月付</span>
-        </p>
-      </div>
+      {size.width > 1919 && (
+        <div className={classes.content}>
+          <span className={classes.agree}>同意聲明</span>{' '}
+          此網站儲存儲如cookie等數據，以啟用網站基本功能與行銷，個人化及分析功能。
+          繼續瀏覽本網站，即表示您同意我們的聲明。 Cookie 政策
+        </div>
+      )}
+      {size.width <= 1919 && size.width > 1023 && (
+        <>
+          <div className={classes.arrow}>𐌡</div>
+          <div className={classes.small_content}>
+            <p>
+              NT$12,609,900<span>/目前總價</span>
+            </p>
+            <p className={classes.monthly_pay}>
+              NT$31,609<span>/最低月付</span>
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
